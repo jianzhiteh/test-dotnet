52306742a782656296deee103a58478a065e0b1f
JianZhiTeh <jianzhiteh@cirosolution.com>
2020-05-22 12:58:20 +0800

    Added setttings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,22 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using test_mvc.Models;
+using test_mvc.Helpers;
 
 namespace test_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<AppSetting> _options;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSetting> options)
         {
             _logger = logger;
+            _options = options;
         }
 
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return _options.Value.TestValue1;
         }
 
         public IActionResult Privacy()
---
+++ Helpers/AppSetting.cs
@@ -0,0 +1,8 @@
+using System;
+
+
+namespace test_mvc.Helpers {
+  public class AppSetting{
+    public string TestValue1 {get;set;}
+  }
+}
